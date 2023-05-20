6fb3d9c63dbb4a5030bfb41b2bda250a8acaddac
Mohammad Rahman <me@shadikur.com>
2023-05-20 13:01:59 +0200

    Slider updated

--- src/components/KeenSlider/KeenSlider.jsx
+++ src/components/KeenSlider/KeenSlider.jsx
@@ -3,7 +3,7 @@ import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import "./style.css"
 
-const KeenSlider = () => {
+const KeenSlider = ({ subtitle }) => {
     const [sliderRef] = useKeenSlider(
         {
             loop: true,
@@ -43,12 +43,9 @@ const KeenSlider = () => {
     return (
         <div>
             <div ref={sliderRef} className="keen-slider">
-                <div className="keen-slider__slide number-slide1">1</div>
-                <div className="keen-slider__slide number-slide2">2</div>
-                <div className="keen-slider__slide number-slide3">3</div>
-                <div className="keen-slider__slide number-slide4">4</div>
-                <div className="keen-slider__slide number-slide5">5</div>
-                <div className="keen-slider__slide number-slide6">6</div>
+                <div className="keen-slider__slide number-slide1">{subtitle}</div>
+                <div className="keen-slider__slide number-slide2">{subtitle}</div>
+                <div className="keen-slider__slide number-slide3">{subtitle}</div>
             </div>
         </div>
     );
--- src/layout/AllToysLayout.jsx
+++ src/layout/AllToysLayout.jsx
@@ -8,7 +8,7 @@ const AllToysLayout = () => {
     return (
         <div>
             <Header></Header>
-            <KeenSlider></KeenSlider>
+            <KeenSlider subtitle={'All Toys'}></KeenSlider>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
