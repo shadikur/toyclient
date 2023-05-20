e770bc9f61f16ea779a28420b30b9b6f4f91da8b
Mohammad Rahman <me@shadikur.com>
2023-05-20 10:57:28 +0200

    Sticky Header

--- src/layout/LandingPages.jsx
+++ src/layout/LandingPages.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from './../components/Footer/Footer';
-import Hero from '../components/Hero/Hero';
+import Sticky from 'react-stickynode';
 
 const LandingPages = () => {
     return (
         <div>
-            <Header></Header>
+            <Sticky top="#header" bottomBoundary="#content">
+                <Header></Header>
+            </Sticky>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
